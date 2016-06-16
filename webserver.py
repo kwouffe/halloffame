b96232638245e59db70af9d87e8487f3d9c046ec
emilien <emilien.le.jamtel@cert.europa.eu>
2016-06-16 03:40:52 +0200

    webserver now dealing with editing json file, still need to work on POST requests

--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # webserver.py - web app to display/edit halloffame.json
+# should be able to launch the vulnerability checking 
 
 """
 Web app to display/edit the halloffame.json file
@@ -16,19 +17,39 @@ import cgi
 import datetime
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import url_for
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 
 
-## json file path
+########## Navbar
+nav = Nav()
+
+@nav.navigation()
+def mynavbar():
+    return Navbar(
+        'Hall of Fame',
+        View('All vulnerabilities', 'index'),
+        View('New vulnerability', 'new_vuln'),
+    )
+######### End Navbar
+
+
+
+## init
 json_file='halloffame.json'
-
 app = Flask(__name__)
 Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.debug = True
+nav.init_app(app)
 #app.run(debug=True)
 #sorted_halloffame = load_sorted_hof()
 
+
+##### Index page (hall of Fame)
 @app.route('/')
 def index():
     return display_hof()
@@ -38,38 +59,102 @@ def display_hof():
     sorted_halloffame = load_sorted_hof()
     return render_template('hof.html', hof_vulns=sorted_halloffame)
 
-############
-### TODO ###
-############
-
-#### Modifying JSON file (add/edit) ######
-@app.route('/edit/<incident_number>')
-def edit_vuln(incident_number):
-    return ''
-
+#### Modifying JSON file (add) ######
 @app.route('/new_vuln/')
 def new_vuln():
-    return ''
+    return render_template('new_vuln.html')
+
+@app.route('/create_vuln/', methods=['POST'])
+def create_vuln():
+    halloffame = load_hof()
+    # creating the new entry
+    # data value is buggy for now. Should find a way to receive POST data to build a proper dictionary for data value
+    new_vuln = {"DO":request.form['DO'],
+        "constituent":request.form['constituent'],
+        "reporter":request.form['reporter'],
+        "report_date":request.form['report_date'],
+        "Incident":request.form['incident_number'],
+        "type":request.form['vuln_type'],
+        "method":request.form['method'],
+        "url":request.form['url'],
+        "data":request.form['data'],
+        "check_string":request.form['check_string'],
+        "scanable":request.form['scanable'],
+        "published":request.form['published'],
+        "patched":"no",
+        "patched_date":"",
+        "last_test":""
+    }
+
+    #adding the entry to the list
+    halloffame.append(new_vuln)
+
+    #writing on the JSON file
+    with open('halloffame.json', 'w') as data:
+        data.write(json.dumps(halloffame, indent=4))
+
+    ### if ok, displaying success page
+    return render_template('done.html')
+
+## function to display/edit all values from a hof entry
+#    ## incident_halloffame should be a list a dictionnaries, each dict being a vuln, all will have common incident number
+#    return render_template('view_vuln.html', hof_vulns=incident_halloffame)
+
+
+@app.route('/view_vuln/<incident_number>')
+def view_vuln(incident_number):
+    halloffame = load_hof()
+
+    #looking for Incident with the specified Incident Number (should be uniq in the future release)
+    list_incident = []
+    for i in range (len(halloffame)):
+            if int(halloffame[i]["Incident"]) == int(incident_number):
+                list_incident.append(halloffame[i])
+
+    #list_incident is the list of dictionnaries (based on JSON file)
+    return render_template('view_vuln.html', hof_vulns=list_incident)
+
+
+@app.route('/update_vuln/', methods=['POST'])
+def update_vuln():
+    halloffame = load_hof()
+
+    print (request.form['reporter'])
+
+    for i in range (len(halloffame)):
+        ## for now url is used as a key - in the future, Incident_number should be uniq
+        if int(halloffame[i]["Incident"]) == int(request.form['incident_number']) and str(halloffame[i]["url"]) == str(request.form['url']):
+            halloffame[i]["DO"] = request.form['DO']
+            halloffame[i]["constituent"] = request.form['constituent']
+            halloffame[i]["reporter"] = request.form['reporter']
+            halloffame[i]["report_date"] = request.form['report_date']
+            halloffame[i]["type"] = request.form['vuln_type']
+            halloffame[i]["method"] = request.form['method']
+            halloffame[i]["url"] = request.form['url']
+            halloffame[i]["data"] = request.form['data']
+            halloffame[i]["check_string"] = request.form['check_string']
+            halloffame[i]["scanable"] = request.form['scanable']
+            halloffame[i]["published"] = request.form['published']
+
+    # writing the JSON file
+    with open('halloffame.json', 'w') as data:
+        data.write(json.dumps(halloffame, indent=4))
+
+    return render_template('done.html')
 
 
 #### Start vulnerability test ####
 #### TODO: modify halloffame.py to a python module ####
-@app.route('/test/<incident_number>')
-def test_vuln(incident_number):
-    return ''
+#@app.route('/test/<incident_number>')
+#def test_vuln(incident_number):
+#    return ''
 
 
 ############################# dealing with json import ###############
-#with open(json_file, 'r') as data:
-#    halloffame = json.load(data)
-
-#sorted_halloffame = sorted(halloffame, key=key_is_date, reverse=True)
-
-
 def load_hof():
     with open(json_file, 'r') as data:
         halloffame = json.load(data)
-
+    return halloffame
 
 def load_sorted_hof():
     with open(json_file, 'r') as data:
